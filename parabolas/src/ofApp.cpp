9f61a5f75ba53a9a03be7391f66ff5f7c6538ba2
Matthew Kenny <matthewkenny@gmail.com>
2019-05-12 21:50:37 -0700

    Formatting and Re-Centering

--- parabolas/src/ofApp.cpp
+++ parabolas/src/ofApp.cpp
@@ -1,12 +1,12 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
 	//ofSetBackgroundAuto(false);
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
 
 }
 
@@ -15,22 +15,27 @@ void drawParabola() {
 
 }
 
-void ofApp::draw(){
+void ofApp::draw() {
+
+	const auto maxLength = 1000;
+	const auto origin = 0;
+	const auto stepSize = maxLength / numSegments;
+
 	ofBackground(0);
 	ofSetColor(ofColor::aqua, 10);
-	ofRotateZ(rotation);
 
-	auto maxLength = 1000;
-	auto origin = 100;
-	auto stepSize = maxLength / numSegments;
+	ofSetRectMode(OF_RECTMODE_CENTER);
+	// Center viz to window.
+	ofTranslate((ofGetWidth() / 2) - (maxLength / 2),
+		(ofGetHeight() / 2) - (maxLength / 2));
+	ofRotateZ(rotation);
 
 	for (auto i = 0; i < numSegments; i++) {
 		auto factor = stepSize * i + origin;
-		auto factor2 = stepSize * (i+1) + origin;
+		auto factor2 = stepSize * (i + 1) + origin;
 		std::vector<glm::vec3> pointVec = { ofVec3f(origin, factor), ofVec3f(factor2, maxLength + origin) };
 		lines.push_back(ofPolyline(pointVec));
 	}
-
 
 	for (auto & l : lines)
 	{
@@ -44,7 +49,7 @@ void ofApp::draw(){
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
 	if (key == 'a')
 		rotation += 10;
 	if (key == 'z')
@@ -58,58 +63,58 @@ void ofApp::keyPressed(int key){
 		numSegments += 10;
 	if (key == 'x')
 		numSegments -= 10;
-		if (numSegments <= 0)
-			numSegments = 10;
+	if (numSegments <= 0)
+		numSegments = 10;
 	if (key == 'q')
 		lines.clear();
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
